db21c450ee3d780a6348b6ef35129ddb743f08eb
Arabic-Dev95 <107544826+Arabic-Dev95@users.noreply.github.com>
2022-06-20 15:51:17 +0300

    Add files via upload

---
+++ Задание бонус-20.06.2022/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_бонус_20._06._2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string path;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(path);
+                sw.WriteLine(textBox1.Text);
+                label1.Text = "Файл сохранён";
+                sw.Close();
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                path = textBox2.Text + ".txt";
+                StreamReader sr = new StreamReader(path);
+                textBox1.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }     
+    }
+}
