4d6d2565ebadf488422c5caf4a468217f787e5a2
Arabic-Dev95 <107544826+Arabic-Dev95@users.noreply.github.com>
2022-06-22 15:46:31 +0300

    Add files via upload

---
+++ Задание 4-22.06.2022-Alfa/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задание_4_22._06._2022
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void CP_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            LB1.Content = CP.SelectedColorText;
+            SolidColorBrush solidColorBrush = new SolidColorBrush();
+            solidColorBrush.Color = CP.SelectedColor.Value;
+            LB1.Background = solidColorBrush;
+        }
+    }
+}
