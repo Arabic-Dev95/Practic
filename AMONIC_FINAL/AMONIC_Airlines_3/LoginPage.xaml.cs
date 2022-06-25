e12d397394cf1a7ecd1f42149cd5babb0227c8a8
Arabic-Dev95 <107544826+Arabic-Dev95@users.noreply.github.com>
2022-06-25 23:10:07 +0300

    Add files via upload

---
+++ AMONIC_FINAL/AMONIC_Airlines_3/LoginPage.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace AMONIC_Airlines_3
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        class InactiveTimer
+        {
+            private DispatcherTimer _timer = new DispatcherTimer();
+            public DispatcherTimer timer { get { return _timer; } }
+
+            private bool _lostKeyboard = true;
+            public bool lostKeyboard { get { return _lostKeyboard; } set { _lostKeyboard = value; } }
+
+            private bool _lostMouse = true;
+            public bool lostMouse { get { return _lostMouse; } set { _lostMouse = value; } }
+
+            private bool _newPage = false;
+            public bool newPage { get { return _newPage; } set { _newPage = value; } }
+
+            public void InitialTimer()
+            {
+                _timer.Interval = new TimeSpan(0, 1, 0);
+            }
+        }
+
+        InactiveTimer inactiveTimer = new InactiveTimer();
+        SqlConnect sqlCon = new SqlConnect();
+        public LoginPage()
+        {
+            InitializeComponent();
+            
+        }
+      
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            inactiveTimer.InitialTimer();
+            inactiveTimer.timer.Tick += Timer_Tick;
+            inactiveTimer.timer.Start();
+            sqlCon.OpenConnect();
+        }
+
+        private void bt_login_Click(object sender, RoutedEventArgs e)
+        {
+            inactiveTimer.timer.Stop();
+
+            inactiveTimer.newPage = true;
+
+            if (!String.IsNullOrWhiteSpace(tb_email.Text) && !String.IsNullOrWhiteSpace(pb_password.Password))
+            {
+                string userInfo = sqlCon.CheckLogin(tb_email.Text, pb_password.Password);
+
+                if(userInfo != null)
+                {
+                    string role = userInfo.Substring(0, userInfo.IndexOf(';'));
+                    bool active = userInfo.Substring(userInfo.IndexOf(';') + 1, userInfo.Length - 1 - userInfo.IndexOf(';')) == "True" ? true : false;
+
+                    if(active)
+                    {
+                        if (role == "User")
+                        {
+                            inactiveTimer.timer.Stop();
+                            FrameNavigate.NavigateTo(new UserPage(tb_email.Text));
+                        }
+                        else if (role == "Administrator")
+                        {
+                            inactiveTimer.timer.Stop();
+                            FrameNavigate.NavigateTo(new AdminPage());
+                        }
+                        else
+                        {
+                            MessageBox.Show("Неизвестная роль.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                    else
+                        MessageBox.Show("Данное действие недоступно. Администратор заблокировал вам доступ.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                    MessageBox.Show("Неправильный логин или пароль.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                
+            }
+            else
+                MessageBox.Show("Заполните пустые поля.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            sqlCon.CloseConnect();
+        }
+
+        private void bt_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы были неактивны в течении одной минуты. Вы все еще с нами?", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Application.Current.Shutdown();
+        }
+
+        private void Page_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            inactiveTimer.lostKeyboard = true;
+
+            if(inactiveTimer.lostMouse && inactiveTimer.lostKeyboard && !inactiveTimer.newPage)
+                inactiveTimer.timer.Start();
+        }
+
+        private void Page_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            inactiveTimer.lostKeyboard = false;
+            inactiveTimer.timer.Stop();
+        }
+
+        private void Page_MouseLeave(object sender, MouseEventArgs e)
+        {
+            inactiveTimer.lostMouse = true;
+
+            if (inactiveTimer.lostMouse && inactiveTimer.lostKeyboard && !inactiveTimer.newPage)
+                inactiveTimer.timer.Start();
+        }
+
+        private void Page_MouseEnter(object sender, MouseEventArgs e)
+        {
+            inactiveTimer.lostMouse = false;
+            inactiveTimer.timer.Stop();
+        }
+    }
+}
---
+++ AMONIC_FINAL/AMONIC_Airlines_3/UserPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows;
+using System.Data;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+namespace AMONIC_Airlines_3
+{
+    /// <summary>
+    /// Логика взаимодействия для UserPage.xaml
+    /// </summary>
+    public partial class UserPage : Page
+    {
+        SqlConnect sqlConnect = new SqlConnect();
+        string email;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        Stopwatch stopwatch = new Stopwatch();
+        string currentTime = string.Empty;
+        string loginTime = string.Empty;
+        string loginDate = string.Empty;
+
+        public UserPage(string em)
+        {
+            InitializeComponent();
+
+            email = em;
+        }
+
+        private void ExitMenuBt_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            FrameNavigate.RemovePage();
+            sqlConnect.OpenConnect();
+
+            sqlConnect.FindActiveSession(email);
+
+            lb_inscription.Content = "Добрый день, " + sqlConnect.FindUserName(email) + ", Добро пожаловать в AMONIC Airlines";
+            lb_crash.Content = "Количество вылетов: " + sqlConnect.FindUserCrashes(email);
+
+            dg_User_Info.ItemsSource = sqlConnect.FindUserTime(email).AsDataView();
+
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+            stopwatch.Start();
+
+            loginTime = DateTime.Now.ToString("HH:mm:ss");
+            loginDate = DateTime.Now.ToString("yyyy-MM-dd");
+            MessageBox.Show(loginDate + " " + loginTime);
+
+            sqlConnect.AddNewUserSession("'" + email + "','" + loginDate + "','" + loginTime + "',null,null,null,0");
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (stopwatch.IsRunning)
+            {
+                TimeSpan ts = stopwatch.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+                lb_time.Content = currentTime;
+                if(ts.Seconds == 10)
+                {
+                    FrameNavigate.NavigateTo(new LoginPage());
+                }
+            }
+        }
+
+        public void SaveData()
+        {
+            string values = "User_Logout_Time = '" + DateTime.Now.ToString("hh:mm:ss") + "', User_Time_Spent = '" + currentTime + "', User_Crash = 0";
+            sqlConnect.UpdateUserTimeInfo(values, loginDate, loginTime);
+        }
+
+        private void CrashMenuBt_Click(object sender, RoutedEventArgs e)
+        {
+
+            Convert.ToInt32("string");
+        }
+    }
+}
